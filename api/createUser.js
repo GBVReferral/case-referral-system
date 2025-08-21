8215e49a64bed36113d54cc3a2fc6ded794cddb0
GBVReferral <casereferralsystem@gmail.com>
2025-08-21 21:45:26 +0630

    --

--- api/createUser.js
+++ api/createUser.js
@@ -1,6 +1,12 @@
 import { adminAuth, adminDb } from "./_firebaseAdmin";
 
 export default async function handler(req, res) {
+  console.log("ENV", {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  hasPrivateKey: !!process.env.FIREBASE_PRIVATE_KEY,
+});
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
