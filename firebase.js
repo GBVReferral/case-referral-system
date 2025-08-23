efa805f3de3377164005d295860165f59a51cd4c
GBVReferral <casereferralsystem@gmail.com>
2025-08-23 20:11:36 +0630

    --

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { initializeFirestore } from "firebase/firestore"; // <-- change here
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArwdDZcOgov0NmnmAdNzrW3bOzWDjRXHk",
+  authDomain: "case-referral-system.firebaseapp.com",
+  projectId: "case-referral-system",
+  storageBucket: "case-referral-system.firebasestorage.app",
+  messagingSenderId: "89787721292",
+  appId: "1:89787721292:web:f21eed81948cdc6c5836e9",
+  measurementId: "G-31EV2X1S8N"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+// 👇 use long-polling instead of WebSockets
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+  useFetchStreams: false,
+});
+
+export const storage = getStorage(app);
